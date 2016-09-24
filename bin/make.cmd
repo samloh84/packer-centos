538c6bad30ea919ee9b5ec55626580d375d7e9ba
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2016-09-24 15:11:00 +0800

    Fixing make batch script Fixing line endings Fixing build template and variable files

--- bin/make.cmd
+++ bin/make.cmd
@@ -1,8 +1,8 @@
 REM To force build, add -force to arguments
 
 pushd ..
-packer.exe build %* centos-7-workstation.json
-packer.exe build %* centos-7-server.json
+packer.exe build -var-file=centos-7-workstation.json %* centos-7.json
+packer.exe build -var-file=centos-7-server.json %* centos-7.json
 
 ovftool.exe output\centos-7-server\centos-7-server.vmx output\centos-7-server.ova
 ovftool.exe output\centos-7-workstation\centos-7-workstation.vmx output\centos-7-workstation.ova
