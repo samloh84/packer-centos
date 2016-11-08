7188cbf8736a514162d0b7580c27d44df6e1b19b
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2016-11-08 21:15:30 +0800

    Refactoring windows build script again.

--- bin/make.cmd
+++ bin/make.cmd
@@ -3,74 +3,107 @@
 REM To force build, add -force to arguments
 REM To build for only one builder, add -only=virtualbox-iso or -only=vmware-iso
 
-SET CENTOS7SERVER=0
-SET CENTOS7WORKSTATION=0
-SET CENTOS6SERVER=0
-SET CENTOS6WORKSTATION=0
-SET CENTOS5SERVER=0
-SET CENTOS5WORKSTATION=0
+SET NO_OPTS=1
 
-echo Building %1
-IF [%1] == [] SET CENTOS7SERVER=1
-IF [%1] == [] SET CENTOS7WORKSTATION=1
-IF [%1] == [] SET CENTOS6SERVER=1
-IF [%1] == [] SET CENTOS6WORKSTATION=1
-IF [%1] == [] SET CENTOS5SERVER=1
-IF [%1] == [] SET CENTOS5WORKSTATION=1
+SET CENTOS_7_SERVER=0
+SET CENTOS_7_WORKSTATION=0
+SET CENTOS_6_SERVER=0
+SET CENTOS_6_WORKSTATION=0
+SET CENTOS_5_SERVER=0
+SET CENTOS_5_WORKSTATION=0
 
-IF "%1" == "all" SET CENTOS7SERVER=1
-IF "%1" == "all" SET CENTOS7WORKSTATION=1
-IF "%1" == "all" SET CENTOS6SERVER=1
-IF "%1" == "all" SET CENTOS6WORKSTATION=1
-IF "%1" == "all" SET CENTOS5SERVER=1
-IF "%1" == "all" SET CENTOS5WORKSTATION=1
+SET REST_ARGS=
 
-IF "%1" == "centos-7" SET CENTOS7SERVER=1
-IF "%1" == "centos-7" SET CENTOS7WORKSTATION=1
-IF "%1" == "centos-6" SET CENTOS6SERVER=1
-IF "%1" == "centos-6" SET CENTOS6WORKSTATION=1
-IF "%1" == "centos-5" SET CENTOS5SERVER=1
-IF "%1" == "centos-5" SET CENTOS5WORKSTATION=1
+:GETOPTS
+SET OPTS_MATCHED=0
+IF "%1" == "all" (
+	SET CENTOS_7_SERVER=1
+	SET CENTOS_7_WORKSTATION=1
+	SET CENTOS_6_SERVER=1
+	SET CENTOS_6_WORKSTATION=1
+	SET CENTOS_5_SERVER=1
+	SET CENTOS_5_WORKSTATION=1
+)
 
-IF "%1" == "centos-7-server" SET CENTOS7SERVER=1
-IF "%1" == "centos-7-workstation" SET CENTOS7WORKSTATION=1
-IF "%1" == "centos-6-server" SET CENTOS6SERVER=1
-IF "%1" == "centos-6-workstation" SET CENTOS6WORKSTATION=1
-IF "%1" == "centos-5-server" SET CENTOS5SERVER=1
-IF "%1" == "centos-5-workstation" SET CENTOS5WORKSTATION=1
+IF "%1" == "centos-7" (
+	SET CENTOS_7_SERVER=1
+	SET CENTOS_7_WORKSTATION=1
+)
 
-set RESTVAR=
-shift
-:loop1
-if "%1"=="" goto after_loop
-set RESTVAR=%RESTVAR% %1
-shift
-goto loop1
+IF "%1" == "centos-6" (
+	SET CENTOS_6_SERVER=1
+	SET CENTOS_6_WORKSTATION=1
+)
 
-:after_loop
+IF "%1" == "centos-5" (
+	SET CENTOS_5_SERVER=1
+	SET CENTOS_5_WORKSTATION=1
+)
+
+IF "%1" == "centos-7-server" (
+	SET CENTOS_7_SERVER=1
+)
+
+IF "%1" == "centos-7-workstation" (
+	SET CENTOS_7_WORKSTATION=1
+)
+
+IF "%1" == "centos-6-server" (
+	SET CENTOS_6_SERVER=1
+)
+
+IF "%1" == "centos-6-workstation" (
+	SET CENTOS_6_WORKSTATION=1
+)
+
+IF "%1" == "centos-5-server" (
+	SET CENTOS_5_SERVER=1
+)
+
+IF "%1" == "centos-5-workstation" (
+	SET CENTOS_5_WORKSTATION=1
+)
+
+
+SHIFT
+IF OPTS_MATCHED EQU 0 SET REST_ARGS=%REST_ARGS% %1
+IF NOT "%1" == "" GOTO GETOPTS
+
+IF NO_OPTS EQU 1 (
+	SET CENTOS_7_SERVER=1
+    SET CENTOS_7_WORKSTATION=1
+    SET CENTOS_6_SERVER=1
+    SET CENTOS_6_WORKSTATION=1
+    SET CENTOS_5_SERVER=1
+    SET CENTOS_5_WORKSTATION=1
+)
+
+
 pushd ..
-IF DEFINED CENTOS7SERVER (
-packer.exe build -var-file=centos-7-server.json %RESTVAR% centos.json
-ovftool.exe --overwrite output\vmware-iso\centos-7-server\centos-7-server.vmx output\centos-7-server.ova
+IF CENTOS_7_SERVER EQU 1 (
+	packer.exe build -var-file=centos-7-server.json %REST_ARGS% centos.json
+	ovftool.exe --overwrite output\vmware-iso\centos-7-server\centos-7-server.vmx output\centos-7-server.ova
 )
-IF DEFINED CENTOS7WORKSTATION (
-packer.exe build -var-file=centos-7-workstation.json %RESTVAR% centos.json
- ovftool.exe --overwrite output\vmware-iso\centos-7-workstation\centos-7-workstation.vmx output\centos-7-workstation.ova
+IF CENTOS_7_WORKSTATION EQU 1 (
+	packer.exe build -var-file=centos-7-workstation.json %REST_ARGS% centos.json
+	ovftool.exe --overwrite output\vmware-iso\centos-7-workstation\centos-7-workstation.vmx output\centos-7-workstation.ova
 )
-IF DEFINED CENTOS6SERVER (
-packer.exe build -var-file=centos-6-server.json %RESTVAR% centos.json
-ovftool.exe --overwrite output\vmware-iso\centos-6-server\centos-7-server.vmx output\centos-7-server.ova
+
+IF CENTOS_6_SERVER EQU 1 (
+	packer.exe build -var-file=centos-6-server.json %REST_ARGS% centos.json
+	ovftool.exe --overwrite output\vmware-iso\centos-6-server\centos-6-server.vmx output\centos-6-server.ova
 )
-IF DEFINED CENTOS6WORKSTATION (
-packer.exe build -var-file=centos-6-workstation.json %RESTVAR% centos.json
-ovftool.exe --overwrite output\vmware-iso\centos-6-workstation\centos-6-workstation.vmx output\centos-6-workstation.ova
+IF CENTOS_6_WORKSTATION EQU 1 (
+	packer.exe build -var-file=centos-6-workstation.json %REST_ARGS% centos.json
+	ovftool.exe --overwrite output\vmware-iso\centos-6-workstation\centos-6-workstation.vmx output\centos-6-workstation.ova
 )
-IF DEFINED CENTOS5SERVER (
-packer.exe build -var-file=centos-5-server.json %RESTVAR% centos.json
-ovftool.exe --overwrite output\vmware-iso\centos-5-server\centos-5-server.vmx output\centos-5-server.ova
+
+IF CENTOS_5_SERVER EQU 1 (
+	packer.exe build -var-file=centos-5-server.json %REST_ARGS% centos.json
+	ovftool.exe --overwrite output\vmware-iso\centos-5-server\centos-5-server.vmx output\centos-5-server.ova
 )
-IF DEFINED CENTOS5WORKSTATION (
-packer.exe build -var-file=centos-5-workstation.json %RESTVAR% centos.json
-ovftool.exe --overwrite output\vmware-iso\centos-5-workstation\centos-5-workstation.vmx output\centos-5-workstation.ova
+IF CENTOS_5_WORKSTATION EQU 1 (
+	packer.exe build -var-file=centos-5-workstation.json %REST_ARGS% centos.json
+	ovftool.exe --overwrite output\vmware-iso\centos-5-workstation\centos-5-workstation.vmx output\centos-5-workstation.ova
 )
 popd
